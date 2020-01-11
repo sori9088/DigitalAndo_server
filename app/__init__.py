469472b2eb2fddf1c1b4176ca6fba988ab5c4dd6
sori9088 <sori9088@naver.com>
2020-01-11 12:10:42 +0700

    initial setup

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from .config import Config
+from .models import db
+from .cli import create_db
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+import stripe
+import uuid
+from dotenv import load_dotenv
+load_dotenv()
+
+
+app = Flask(__name__)
+CORS(app) 
+app.config.from_object(Config)
+app.cli.add_command(create_db)
+db.init_app(app)
+migrate = Migrate(app, db) 
+
+
+
+# @app.route("/")
+# def 
