89e6163fdbb2039f84e85edcabb182e370587b7d
Chloe <chloe.jihye@gmail.com>
2020-01-12 01:04:15 +0700

    Revert "updated database model and api for post data"
    This reverts commit 5bcf10734892c5641cd9819782781de37fedfd51.

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,11 @@ from .cli import create_db
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-import datetime
+import stripe
 import uuid
 from dotenv import load_dotenv
 load_dotenv()
+
 
 app = Flask(__name__)
 CORS(app) 
@@ -17,40 +18,10 @@ app.cli.add_command(create_db)
 db.init_app(app)
 migrate = Migrate(app, db) 
 
-@app.route("/", methods=["POST","GET"])
-def root():
-    return jsonify(
-        {"success": True}
-    )
+
 
 @app.route("/getinfo", methods=["POST","GET"])
 def getinfo():
     return jsonify(
         {"success":True}
         )
-
-@app.route("/send_issue", methods=["POST", "GET"])
-def send_issue():
-    if request.method == "POST":
-        data = request.get_json()
-        data1 = request.get_json()['detail1']
-        data2 = request.get_json()['detail2']
-        
-        new_issue = Issue(
-            line_id = data['line_id'],
-            issue_type = datetime.datetime.now(),
-            detail1 = data1['title'],
-            detail2 = data2['title'],
-            remark = data['remark'],
-            submit_date = datetime.datetime.now(),
-        )
-        db.session.add(new_issue)
-        db.session.commit()
-        issues = Issue.query.all()
-        
-        res = {
-            "success": True,
-            "issues": [issue.render() for issue in issues]
-        }
-       
-    return jsonify(res) 
--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ import datetime
 from sqlalchemy.sql import func
 
 
+
+
 db = SQLAlchemy()
 
 
@@ -13,7 +15,7 @@ class Issue(db.Model):
     line_id = db.Column(db.String(256))
     issue_type = db.Column(db.String(256))
     detail1 = db.Column(db.String(256))
-    detail2 = db.Column(db.String(256))
+    datail2 = db.Column(db.String(256))
     remark = db.Column(db.Text)
     status = db.Column(db.String(256))
     submit_date = db.Column(db.DateTime(timezone=True), server_default=func.now())
@@ -21,16 +23,3 @@ class Issue(db.Model):
     finish_date =  db.Column(db.DateTime(timezone=True), server_default=func.now())
     duration = db.Column(db.Integer)
 
-    def render(self):
-            return {
-                    "line_id" : self.line_id,
-                    "issue_type" : self.issue_type,
-                    "detail1" : self.detail1,
-                    "detail2" : self.detail2,
-                    "remark" : self.remark,
-                    "status" : self.status,
-                    "submit_date" : self.submit_date,
-                    "start_date" : self.start_date,
-                    "finish_date" :  self.finish_date,
-                    "duration" : self.duration
-            }
