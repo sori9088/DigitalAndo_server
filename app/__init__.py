589fcae2cca3d2eb376d0ad304cc8ddfd8f17a40
sori9088 <sori9088@naver.com>
2020-01-12 02:06:48 +0700

    the end of day

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from .config import Config
-from .models import db
+from .models import db, Issue
 from .cli import create_db
 from flask_migrate import Migrate
 from flask_cors import CORS
@@ -8,6 +8,8 @@ from flask_sqlalchemy import SQLAlchemy
 import stripe
 import uuid
 from dotenv import load_dotenv
+from sqlalchemy import or_
+
 load_dotenv()
 
 
@@ -20,5 +22,82 @@ migrate = Migrate(app, db)
 
 
 
-# @app.route("/")
-# def 
+@app.route("/")
+def root():
+    return jsonify({"success": True})
+
+
+@app.route("/getinfo", methods=["POST","GET"])
+def getinfo():
+
+    issues = Issue.query.filter(Issue.status!="done").all()
+    res = {
+        "success" : True,
+        "issues" : [ {
+            "id" : issue.id,
+            "line_id" : issue.line_id,
+            "issue_type" :issue.issue_type,
+            "detail1" : issue.detail1,
+            "detail2" : issue.detail2,
+            "remark" : issue.remark,
+            "status" : issue.status,
+            "submit_date" : issue.submit_date,
+            "start_date" : issue.start_date,
+            } for issue in issues] 
+        }
+    return jsonify(res)
+
+
+
+@app.route("/doing", methods=["POST", "GET"])
+def doing():
+    if request.method =="POST" :
+        data = request.get_json()
+        issue = Issue.query.filter_by(id=data['id']).first()
+        issue.status = "doing"
+
+        db.session.commit()
+            
+        issues = Issue.query.filter(Issue.status!="done").all()
+        res = {
+            "success" : True,
+            "issues" : [ {
+                "id" : issue.id,
+                "line_id" : issue.line_id,
+                "issue_type" :issue.issue_type,
+                "detail1" : issue.detail1,
+                "detail2" : issue.detail2,
+                "remark" : issue.remark,
+                "status" : issue.status,
+                "submit_date" : issue.submit_date,
+                "start_date" : issue.start_date,
+                } for issue in issues] 
+            }
+        return jsonify(res)
+
+
+@app.route("/done", methods=["POST", "GET"])
+def done():
+    if request.method =="POST" :
+        data = request.get_json()
+        issue = Issue.query.filter_by(id=data['id']).first()
+        issue.status = "done"
+
+        db.session.commit()
+            
+        issues = Issue.query.filter(Issue.status!="done").all()
+        res = {
+            "success" : True,
+            "issues" : [ {
+                "id" : issue.id,
+                "line_id" : issue.line_id,
+                "issue_type" :issue.issue_type,
+                "detail1" : issue.detail1,
+                "detail2" : issue.detail2,
+                "remark" : issue.remark,
+                "status" : issue.status,
+                "submit_date" : issue.submit_date,
+                "start_date" : issue.start_date,
+                } for issue in issues] 
+            }
+        return jsonify(res)
--- app/models.py
+++ app/models.py
@@ -7,3 +7,32 @@ from sqlalchemy.sql import func
 
 
 db = SQLAlchemy()
+
+
+class Issue(db.Model):
+    __tablename__='issues'
+    id = db.Column(db.Integer, primary_key=True)
+    line_id = db.Column(db.String(256))
+    issue_type = db.Column(db.String(256))
+    detail1 = db.Column(db.String(256))
+    detail2 = db.Column(db.String(256))
+    remark = db.Column(db.Text)
+    status = db.Column(db.String(256), default="null")
+    submit_date = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    start_date =  db.Column(db.DateTime(timezone=True), server_default=func.now())
+    finish_date =  db.Column(db.DateTime(timezone=True), server_default=func.now())
+    duration = db.Column(db.Integer)
+
+    def render(self):
+        return {
+                "line_id" : self.line_id,
+                "issue_type" :self.issue_type,
+                "detail1" : self.detail1,
+                "detail2" : self.detail2,
+                "remark" : self.remark,
+                "status" : self.status,
+                "submit_date" : self.submit_date,
+                "start_date" : self.start_date,
+                "finish_date" :  self.finish_date,
+                "duration" : self.duration
+            }
